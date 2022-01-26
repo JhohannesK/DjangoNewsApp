4c7191ed71c4b9251473bcdc934119aca249bfab
Domeh John <jdomeh77@gmail.com>
2022-01-26 15:11:17 +0000

    initial commit

---
+++ newsApp/views.py
@@ -0,0 +1,26 @@
+from operator import ne
+from django.shortcuts import render
+from newsapi import NewsApiClient
+
+
+def index(request):
+    newsApi = NewsApiClient(api_key="c74a92a23a7b468fb8b0848c7635d984")
+    headlines = newsApi.get_everything(sources='bbc-news, cnn', language='en', page=3)
+    articles = headlines['articles']
+    desc = []
+    news = []
+    image = []
+    url = []
+    source= []
+
+    for i in range(len(articles)):
+        article = articles[i]
+        desc.append(article['description'])
+        news.append(article['title'])
+        image.append(article['urlToImage'])
+        source.append(article['source'])
+        url.append(article['url'])
+    mylist = zip(desc, news, image, url, source)
+    context = {'mylist': mylist}
+
+    return render(request, 'newsApp/index.html', context)
