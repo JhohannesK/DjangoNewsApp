192570a54ae76508ce70ead8a8173a2dda051dd8
Domeh John <jdomeh77@gmail.com>
2022-01-28 03:06:07 +0000

    touches

--- newsApp/views.py
+++ newsApp/views.py
@@ -4,23 +4,25 @@ from newsapi import NewsApiClient
 
 
 def index(request):
-    # newsApi = NewsApiClient(api_key="c74a92a23a7b468fb8b0848c7635d984")
-    # headlines = newsApi.get_everything(sources='bbc-news, cnn', language='en', page=3)
-    # articles = headlines['articles']
-    # desc = []
-    # news = []
-    # image = []
-    # url = []
-    # source= []
+    newsApi = NewsApiClient(api_key="c74a92a23a7b468fb8b0848c7635d984")
+    headlines = newsApi.get_everything(sources='bbc-news, cnn',
+                                       language='en',
+                                       page=3)
+    articles = headlines['articles']
+    desc = []
+    news = []
+    image = []
+    url = []
+    source = []
 
-    # for i in range(len(articles)):
-    #     article = articles[i]
-    #     desc.append(article['description'])
-    #     news.append(article['title'])
-    #     image.append(article['urlToImage'])
-    #     source.append(article['source'])
-    #     url.append(article['url'])
-    # mylist = zip(desc, news, image, url, source)
-    # context = {'mylist': mylist}
+    for i in range(len(articles)):
+        article = articles[i]
+        desc.append(article['description'])
+        news.append(article['title'])
+        image.append(article['urlToImage'])
+        source.append(article['source'])
+        url.append(article['url'])
+    mylist = zip(desc, news, image, url, source)
+    context = {'mylist': mylist}
 
-    return render(request, 'newsApp/index.html')
+    return render(request, 'newsApp/index.html', context)
